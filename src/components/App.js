fd2a59ccafb47ffa3172d3d35031eb71013a9361
Roi Mass <rmass@atlasprofessionals.com>
2020-11-16 19:44:43 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react'
+import Editor from './Editor'
+import useLoacalStorage from '../hooks/useLocalStorage'
+
+function App() {
+  const [html, setHtml] = useLoacalStorage('html', '')
+  const [css, setCSS] = useLoacalStorage('css', '')
+  const [js, setJs] = useLoacalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(
+        `
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+        `
+      )
+    }, 500)
+    
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
+
+
+  return (
+  <>
+    <div className="pane top-pane">
+      <Editor language="xml" displayName="HTML" value={html} onChange={setHtml}/>
+      <Editor language="css" displayName="CSS" value={css} onChange={setCSS} />
+      <Editor language="javascript" displayName="JS" value={js} onChange={setJs} />
+    </div>
+
+    <div className="pane">
+      <iframe 
+        srcDoc={srcDoc}
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
+    </div>
+  </>
+  )
+}
+
+export default App;
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+
+const PREFIX = 'codepen-clone-'
+
+export default function useLocalStorage(key, initialValue) {
+    const prefixKey = PREFIX + key
+
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixKey)
+
+        if (jsonValue != null) return JSON.parse(jsonValue)
+
+        if (typeof initialValue === 'function'){
+            return initialValue()
+        } else {
+            return initialValue
+        }
+    })
+
+    useEffect(() => {
+        localStorage.setItem(prefixKey, JSON.stringify(value))
+    }, [prefixKey, value])
+
+    return [value, setValue]
+}
